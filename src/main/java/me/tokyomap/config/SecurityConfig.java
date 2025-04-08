30833a6e7927e91a9195d46d4f04d8bcbb9bdc40
RYUSEULKI <erenos33@gmail.com>
2025-04-08 13:52:52 +0900

    update

---
+++ src/main/java/me/tokyomap/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package me.tokyomap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+}
