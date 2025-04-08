fe01c327d3ca673328594e64e9fda799edc59214
RYUSEULKI <erenos33@gmail.com>
2025-04-08 12:15:05 +0900

    🎉 new project

---
+++ src/main/java/me/tokyomap/TokyomapApplication.java
@@ -0,0 +1,13 @@
+package me.tokyomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokyomapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokyomapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/tokyomap/TokyomapApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tokyomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokyomapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
